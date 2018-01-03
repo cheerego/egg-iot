a0779780e547a46d10cf8acfa3d5b8846e66eddc
placeless <placeless@catchme.com.nc>
2018-01-03 16:35:39 +0800

    package.json

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 module.exports = app => {
-    app.addSingleton('iot', createIot);
-}
+  app.addSingleton('iot', createIot);
+};
 
 function createIot(config, app) {
-    // 创建实例
-    const IOT = require('./iot');
-    return new IOT(config);
+  // 创建实例
+  const IOT = require('./iot');
+  const iot = new IOT(config);
+  app.beforeStart(async () => {
+    iot.GetDeviceShadow('pRb1mK8CTPJ', 'CATCH_100010').then(console.log);
+  });
+  return new IOT(config);
 }
--- test/fixtures/apps/iot-test/app/controller/home.js
+++ test/fixtures/apps/iot-test/app/controller/home.js
@@ -4,7 +4,7 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = 'hi, ' + this.app.plugins.iot.name;
+      this.ctx.body =await this.app.iot.GetDeviceShadow('pRb1mK8CTPJ', 'CATCH_100010');
   }
 }
 
