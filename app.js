5203c239f276e9064d92b61c7f9cb9b8be4babc8
placeless <placeless@catchme.com.nc>
2018-01-03 16:49:45 +0800

    package.json

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const IOT = require('./iot');
 module.exports = app => {
-
     app.beforeStart(async () => {
-        let iot = new IOT(app.config.iot);
-        app.iot = iot;
+        app.iot = new IOT(app.config.iot);
     });
 };
 
